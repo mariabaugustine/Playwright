6bf75d60a4bd415c7368f5bfe4728736e6ab9522
maria.baugustine <maria.baugustine@ust.com>
2024-01-02 15:04:53 +0530

    modified

--- ConsoleAppProject/Program.cs
+++ ConsoleAppProject/Program.cs
@@ -10,5 +10,17 @@ await using var browser=await playwright.Chromium.LaunchAsync();
 //Page instance
 var context=await browser.NewContextAsync();
 var page=await context.NewPageAsync();
-
+Console.WriteLine("Opened Browser");
 await page.GotoAsync("https://www.google.com");
+Console.WriteLine("Page Loaded");
+
+string title=await page.TitleAsync();
+Console.WriteLine(title);
+//await page.GetByTitle("Search").FillAsync("hp laptop");
+await page.Locator("#APjFqb").FillAsync("Selenium");
+Console.WriteLine("Typed Search Text");
+
+await page.Locator("(//input[@value='Google Search'])[2]").ClickAsync();
+Console.WriteLine("Clicked");
+title=await page.TitleAsync();
+Console.WriteLine(title);
