85e096086f6cdfd221b58be51c1d07248dad948b
maria.baugustine <maria.baugustine@ust.com>
2024-01-02 10:28:54 +0530

    Add project files.

---
+++ ConsoleAppProject/Program.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Playwright;
+
+//playwright startup
+
+using var playwright =await Playwright.CreateAsync();
+//Launch browser
+
+await using var browser=await playwright.Chromium.LaunchAsync();
+
+//Page instance
+var context=await browser.NewContextAsync();
+var page=await context.NewPageAsync();
