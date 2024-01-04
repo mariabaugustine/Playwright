4b9653996e0ee49b003110e1d48d32f7d47c3a85
maria.baugustine <maria.baugustine@ust.com>
2024-01-04 12:34:57 +0530

    modified

---
+++ PlayWrightPOM/PWTests/Pages/NewLoginPage.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Playwright;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlayWrightPOM.PWTests.Pages
+{
+    internal class NewLoginPage
+    {
+        private IPage _page;
+        private ILocator LinkLogin=> _page.Locator(selector: "text=Login");
+        private ILocator InpUserName => _page.Locator(selector: "#UserName");
+        private ILocator InpPassword=> _page.Locator(selector: "#Password");
+        private ILocator BtnLogin => _page.Locator(selector: "input", new PageLocatorOptions
+        {
+            HasTextString = "Log in"
+        });
+        private ILocator LnkWelMess=> _page.Locator(selector: "text='Hello admin!'");
+        //constructor
+        public NewLoginPage(IPage page)=>_page=page;
+        
+
+        public async Task ClickLoginLink()=> await LinkLogin.ClickAsync();
+        public async Task Login(string username,string password)
+        {
+            await InpUserName.FillAsync(username);
+            await InpPassword.FillAsync(password);
+            await BtnLogin.ClickAsync();
+        }
+        public async Task<bool> CheckWelcomeMessage() => await LnkWelMess.IsVisibleAsync();
+          
+    }
+}
--- PlayWrightPOM/PWTests/Tests/LoginPageTest.cs
+++ PlayWrightPOM/PWTests/Tests/LoginPageTest.cs
@@ -6,15 +6,30 @@ namespace PlayWrightPOM.PWTests.Tests
 {
     public class LoginPageTest:PageTest
     {
+        Dictionary<string, string> Properties;
+        private void ReadConfigSettings()
+        {
+            Properties = new Dictionary<string, string>();
+            string? currdir = Directory.GetParent(@"../../../")?.FullName;
+            string fileName = currdir + "/configsettings/config.properties";
+            string[] lines = File.ReadAllLines(fileName);
+            foreach (string line in lines)
+            {
+                if (!string.IsNullOrWhiteSpace(line) && line.Contains('='))
+                {
+                    string[] parts = line.Split('=');
+                    string key = parts[0].Trim();
+                    string value = parts[1].Trim();
+                    Properties[key] = value;
+                }
+            }
+        }
         [SetUp]
         public async Task Setup()
         {
+            ReadConfigSettings();
             Console.WriteLine("Opened Browser");
-            await Page.GotoAsync("http://eaapp.somee.com/", new PageGotoOptions()
-            {
-                Timeout = 3000,
-                WaitUntil = WaitUntilState.DOMContentLoaded
-            });
+            await Page.GotoAsync(Properties["baseUrl"]);
             Console.WriteLine("Page Loaded");
         }
 
@@ -23,7 +38,8 @@ namespace PlayWrightPOM.PWTests.Tests
         [TestCase("admin", "xxxxx")]
         public async Task  LoginTest(string username,string password)
         {
-            LoginPage loginPage=new (Page);
+            NewLoginPage loginPage = new(Page);
+            //LoginPage loginPage=new (Page);
             await loginPage.ClickLoginLink();
             await loginPage.Login(username,password);
             Assert.IsTrue(await loginPage.CheckWelcomeMessage());
